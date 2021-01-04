76163f3f2456bb44606efa1302e5ff32960c7c11
Jamie Davis <jamie.davis@ao.com>
2021-01-04 11:46:59 +0000

    Setting up our Serverless infrastructure

---
+++ infrastructure/lib/index.js
@@ -0,0 +1,12 @@
+import CognitoStack from "./CognitoStack";
+import DynamoDBStack from "./DynamoDBStack";
+import S3Stack from "./S3Stack";
+
+// Add stacks
+export default function main(app) {
+    new DynamoDBStack(app, "dynamodb");
+
+    const s3 = new S3Stack(app, "s3");
+
+    new CognitoStack(app, "cognito", { bucketArn: s3.bucket.bucketArn });
+}
---
+++ infrastructure/test/DynamoDBStack.test.js
@@ -0,0 +1,17 @@
+import * as sst from "@serverless-stack/resources";
+
+import { expect, haveResource } from "@aws-cdk/assert";
+
+import DynamoDBStack from "../lib/DynamoDBStack";
+
+test("Test Stack", () => {
+  const app = new sst.App();
+  // WHEN
+  const stack = new DynamoDBStack(app, "test-stack");
+  // THEN
+  expect(stack).to(
+    haveResource("AWS::DynamoDB::Table", {
+      BillingMode: "PAY_PER_REQUEST",
+    })
+  );
+});
